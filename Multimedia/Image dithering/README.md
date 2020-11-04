13cc47f8afdcc0c9d539ae59e8916e548e0e15cf
JunGi_Kim <junhardstudy@knu.ac.kr>
2020-11-04 18:39:58 +0900

    commit

---
+++ Multimedia/Image dithering/README.md
@@ -0,0 +1,31 @@
+# Error diffusion dithering
+
+Lenna image에 대해 openCV 라이브러리를 이용하여, Lenna image를 읽어 들이고, dithering을 적용하기 전과 적용한 후에 대한 결과를 표시하는
+프로그램입니다.
+
+## Dithering
+
+이론
+
+특정 디바이스에서는 성능이 부족해서, 원이미지나 원영상을 똑같이 출력하기가 힘든 경우가 있습니다. 가령 예를 들어, 흑백 프린터기난 휴대용 게임기와 같은 디바이스는 성능이 제약되어
+original media의 모든 색상이 사용이 불가능합니다.
+
+이런 경우, 다른 색상이나 픽셀 값을 섞어서 시각적으로 비슷한 색상을 내도록하는 것이 dithering입니다.
+
+알고리즘 전개 방식은 하나의 픽셀 값은 2by2, 또는 4by4 더 큰 픽셀 패턴으로 바꾸는 것입니다.
+
+Example) 2bits 색상 값(0, 1, 2, 3)을 가질 수 있는 경우 아래와 같은 dithering matrix를 이용하여 표현 할 수 있습니다.
+
+![equ](https://latex.codecogs.com/gif.latex?log(y)=\beta_0&space;&plus;&space;\beta_1&space;x&space;&plus;&space;u)
+
+## Error diffusion dithering
+
+dithering시 발생하는 픽셀 값에 대한 quantization error를 줄이는 방법입니다.
+
+어느 픽셀에서 발생한 error(noise)를 주변 픽셀로 확산시킵니다.
+
+
+이론
+
+## 소스코드
+
---
+++ Multimedia/Image dithering/source code/dithering.cpp
@@ -0,0 +1,88 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+	int i, j, t;
+	int error;
+	Mat img;
+	Mat gray_img;
+	Mat after_img;
+	Mat after_col_img;
+
+	img = imread("Lenna.png", IMREAD_COLOR);
+	after_col_img = imread("Lenna.png", IMREAD_COLOR);
+
+	cvtColor(img, gray_img, COLOR_BGR2GRAY);
+	cvtColor(img, after_img, COLOR_BGR2GRAY);
+	imwrite("gray image.jpg", gray_img);
+
+	/************************************************************************
+		gray image에 대해 error diffusion dithering algorithm을 적용합니다.
+	*************************************************************************/
+	for (i = 0; i < after_img.rows - 1; i++) {
+		for (j = 1; j < after_img.cols - 1; j++) {
+			if (after_img.at<uchar>(i, j) > 127)error = after_img.at<uchar>(i, j) - 255;
+			else error = after_img.at<uchar>(i, j);
+			after_img.at<uchar>(i, j + 1) += (uchar)((error * 7) / 16);
+			after_img.at<uchar>(i + 1, j - 1) += (uchar)((error * 3) / 16);
+			after_img.at<uchar>(i + 1, j) += (uchar)((error * 5) / 16);
+			after_img.at<uchar>(i + 1, j + 1) += (uchar)((error * 1) / 16);
+		}
+	}
+	for (i = 0; i < after_img.rows; i++) {
+		for (j = 0; j < after_img.cols; j++) {
+			if (after_img.at<uchar>(i, j) <= 127)after_img.at<uchar>(i, j) = 0;
+			else after_img.at<uchar>(i, j) = 255;
+		}
+	}
+
+	imwrite("result.jpg", after_img);
+
+	/************************************************************************
+		color image에 대해 error diffusion dithering algorithm을 적용합니다.
+	*************************************************************************/
+
+	for (t = 0; t < 3; t++) {
+		for (i = 0; i < after_col_img.rows - 1; i++) {
+			for (j = 1; j < after_col_img.cols - 1; j++) {
+
+				if (after_col_img.at<Vec3b>(i, j)[t] > 127)error = after_col_img.at<Vec3b>(i, j)[t] - 255;
+				else error = after_col_img.at<Vec3b>(i, j)[t];
+				after_col_img.at<Vec3b>(i, j + 1)[t] += (error * 7) / 16;
+				after_col_img.at<Vec3b>(i + 1, j - 1)[t] += (error * 3) / 16;
+				after_col_img.at<Vec3b>(i + 1, j)[t] += (error * 5) / 16;
+				after_col_img.at<Vec3b>(i + 1, j + 1)[t] += (error * 1) / 16;
+			}
+
+		}
+	}
+
+
+	for (t = 0; t < 3; t++) {
+		for (i = 0; i < after_col_img.rows; i++)
+			for (j = 0; j < after_col_img.cols; j++) {
+				if (after_col_img.at<Vec3b>(i, j)[t] <= 127) after_col_img.at<Vec3b>(i, j)[t] = 0;
+				else after_col_img.at<Vec3b>(i, j)[t] = 255;
+			}
+	}
+
+
+	namedWindow("original_dip", WINDOW_NORMAL);
+	namedWindow("gray_dip", WINDOW_NORMAL);
+	namedWindow("after_dip", WINDOW_NORMAL);
+	namedWindow("after_col_dip", WINDOW_NORMAL);
+	imshow("original_dip", img);
+	imshow("gray_dip", gray_img);
+	imshow("after_dip", after_img);
+	imshow("after_col_dip", after_col_img);
+	imwrite("result_col.jpg", after_col_img);
+	waitKey(0);
+
+
+
+	return 0;
+}
